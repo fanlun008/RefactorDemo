90f0fe42c4b82f0ad83acc514b6e72aa860ef1cb
fanlun008 <549734423@qq.com>
2019-08-12 20:26:30 +0800

    refactor: extract item's name judgment

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -1,5 +1,10 @@
 package gildedRose;
 public class GildedRose {
+    public static final String AGED = "Aged Brie";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String OTHERNAME = "othername";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,10 +13,10 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].isAgedName()
-                    && !items[i].isBackstageName()) {
+            if (!isAgedName(items[i])
+                    && !isBackstageName(items[i])) {
                 if (items[i].quality > 0) {
-                    if (!items[i].isSulfurasName()) {
+                    if (!isSulfurasName(items[i])) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -19,7 +24,7 @@ public class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].isBackstageName()) {
+                    if (isBackstageName(items[i])) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -35,15 +40,15 @@ public class GildedRose {
                 }
             }
 
-            if (!items[i].isSulfurasName()) {
+            if (!isSulfurasName(items[i])) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].isAgedName()) {
-                    if (!items[i].isBackstageName()) {
+                if (!isAgedName(items[i])) {
+                    if (!isBackstageName(items[i])) {
                         if (items[i].quality > 0) {
-                            if (!items[i].isSulfurasName()) {
+                            if (!isSulfurasName(items[i])) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
@@ -57,5 +62,17 @@ public class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isAgedName(Item item) {
+        return item.name == AGED;
+    }
+
+    private boolean isBackstageName(Item item) {
+        return item.name == BACKSTAGE;
+    }
+
+    private boolean isSulfurasName(Item item) {
+        return item.name == SULFURAS;
     }
 }
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -2,11 +2,6 @@ package gildedRose;
 
 public class Item {
 
-    public static final String AGED = "Aged Brie";
-    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String OTHERNAME = "othername";
-
     public String name;
 
     public int sellIn;
@@ -22,17 +17,5 @@ public class Item {
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
-
-    public boolean isAgedName() {
-        return this.name.equals(AGED);
-    }
-
-    public boolean isBackstageName() {
-        return this.name.equals(BACKSTAGE);
-    }
-
-    public boolean isSulfurasName() {
-        return this.name.equals(SULFURAS);
     }
 }
